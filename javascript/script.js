f46693d258477c7b1e39328656631d7b2f1121ff
degreatdev <domikdegreat@gmail.com>
2022-06-24 14:54:04 +0100

    update on js

--- javascript/script.js
+++ javascript/script.js
@@ -10,7 +10,8 @@ function length_of_object(objectArg) {
 
 };
 
-vinix = ['C', 'MA', 'BA', 'PG', 'CSCO', 'VZ', 'PFE', 'HD', 'INTC']
+//A FUNCTION THAT PRINTS EVERYTHING IN A LIST
+var vinix = ['C', 'MA', 'BA', 'PG', 'CSCO', 'VZ', 'PFE', 'HD', 'INTC']
 listArray = vinix;
   (function printsAll (listObject) {
    for (var i = 0; i < listObject.length; i++) {
@@ -19,3 +20,16 @@ listArray = vinix;
    return True
  }
 (listArray));
+
+//OBJECT WITH FUNCTION OF CHANGING NAME
+var changeName = {
+  namestuds: "jim",
+  student: this.name,
+  changeToTim : function changing(nameRecieved) {
+    ChangeName.namestuds = nameRecieved;
+  },
+  alertMsg:function alerting() {
+      console.log("changed name to " + this.namestuds )
+  }
+
+}
